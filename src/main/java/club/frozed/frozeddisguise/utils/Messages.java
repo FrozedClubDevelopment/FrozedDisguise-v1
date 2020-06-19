f951f4ab9b5993b398d9d3058ae8042f746cbd88
Elb1to <elbitomc@gmail.com>
2020-06-18 21:54:30 -0500

    Update Messages.java

--- src/main/java/club/frozed/frozeddisguise/utils/Messages.java
+++ src/main/java/club/frozed/frozeddisguise/utils/Messages.java
@@ -2,21 +2,8 @@ package club.frozed.frozeddisguise.utils;
 
 import org.bukkit.ChatColor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Messages {
-
     public static String CC(String msg) {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
-
-    public static List<String> CC(List<String> msgs) {
-        List<String> result = new ArrayList<>();
-        for (String msg : msgs) {
-            result.add(ChatColor.translateAlternateColorCodes('&', msg));
-        }
-        return result;
-    }
-
 }
