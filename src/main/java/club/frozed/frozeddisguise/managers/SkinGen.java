3d25033faa6200918252a6c7c217f70bf25b2be6
Elb1to <elbitomc@gmail.com>
2020-06-26 18:10:20 -0500

    Reformatted this shit (check if it causes issues in the future)

--- src/main/java/club/frozed/frozeddisguise/managers/SkinGen.java
+++ src/main/java/club/frozed/frozeddisguise/managers/SkinGen.java
@@ -21,18 +21,17 @@ public class SkinGen {
 
     public static String generate() {
         Random random = new Random();
-        return SkinGen.skins.get(random.nextInt(SkinGen.skins.size()));
+        return skins.get(random.nextInt(skins.size()));
     }
 
     static {
-        SkinGen.data = new HashMap<>();
-        SkinGen.skins = Arrays.asList(
-                "_rsu:Killer", "E_Girl:E-girl", "burningbridge:Link", "BackupDancer:L", "Arro:Arabian", "bennyknight:Badman", "Cheggthemegg:Ricardo Milos",
-                "DonaldTrumpinho:Trump", "CocoDeMedellin:BlackGoku", "SheAGoldDigger:BlueGoku", "DJ_Blazes:Enderman", "Marcel:Marcel", "Zwergoor:Emoji",
-                "HikakinGames:Youtuber", "loudoggydog3010:XXX", "Killer0dead:Rem", "SolluxCaptor:Nezuko", "weebh0e:Shinobu");
+        data = new HashMap<>();
+        skins = Arrays.asList("_rsu:Killer", "E_Girl:E-girl", "burningbridge:Link", "BackupDancer:L", "Arro:Arabian", "bennyknight:Badman", "Cheggthemegg:Ricardo Milos",
+                "DonaldTrumpinho:Trump", "CocoDeMedellin:BlackGoku", "SheAGoldDigger:BlueGoku", "DJ_Blazes:Enderman", "Marcel:Marcel", "Zwergoor:Emoji", "HikakinGames:Youtuber",
+                "loudoggydog3010:XXX", "Killer0dead:Rem", "SolluxCaptor:Nezuko", "weebh0e:Shinobu");
 
-        SkinGen.menu = Bukkit.createInventory(null, 18, Messages.CC("&8Select an Skin"));
-        for (String skin : SkinGen.skins) {
+        menu = Bukkit.createInventory(null, 18, Messages.CC("&8Select an Skin"));
+        for (String skin : skins) {
             ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
             SkullMeta skullmeta = (SkullMeta) skull.getItemMeta();
             String nick = skin.split(":")[0];
@@ -40,8 +39,8 @@ public class SkinGen {
             skullmeta.setOwner(nick);
             skullmeta.setDisplayName(ChatColor.AQUA + display);
             skull.setItemMeta(skullmeta);
-            SkinGen.menu.addItem(skull);
-            SkinGen.data.put(skull.getItemMeta().getDisplayName(), nick + ":" + display);
+            menu.addItem(skull);
+            data.put(skull.getItemMeta().getDisplayName(), nick + ":" + display);
         }
     }
 
