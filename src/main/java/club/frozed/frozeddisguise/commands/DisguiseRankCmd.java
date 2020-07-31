570384cc078abe93f2efa19aa11d17ce043e6055
Elb1to <elbitomc@gmail.com>
2020-07-31 17:00:48 -0500

    Created DisguiseRankCmd and made msg's configurable

---
+++ src/main/java/club/frozed/frozeddisguise/commands/DisguiseRankCmd.java
@@ -0,0 +1,36 @@
+package club.frozed.frozeddisguise.commands;
+
+import club.frozed.frozeddisguise.FrozedDisguise;
+import club.frozed.frozeddisguise.ranks.RanksMenu;
+import club.frozed.frozeddisguise.utils.Messages;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+public class DisguiseRankCmd implements CommandExecutor, Listener {
+
+    boolean allowDisguiseRankWithoutBeingDisguised = FrozedDisguise.getInstance().getConfig().getBoolean("BOOLEANS.ALLOW-DISGUISE-RANK-WITHOUT-BEING-DISGUISED");
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!sender.hasPermission("frozed.disguise.ranks")) {
+            sender.sendMessage(Messages.CC(FrozedDisguise.getInstance().getConfig().getString("MESSAGES.NO-PERMISSION-TO-USE")));
+            return true;
+        }
+
+        if (!(sender instanceof Player)) {
+            return true;
+        }
+
+        Player player = (Player) sender;
+        if (allowDisguiseRankWithoutBeingDisguised) {
+            new RanksMenu().openMenu(player);
+        } else {
+            player.sendMessage(Messages.CC(FrozedDisguise.getInstance().getConfig().getString("MESSAGES.DISGUISE-REQUIRED-FOR-RANK")));
+        }
+
+        return true;
+    }
+}
