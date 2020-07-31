2526f7c63a442ca8064bb238caf79607e20ffb17
Elb1to <elbitomc@gmail.com>
2020-07-31 16:53:50 -0500

    Added ItemBuilder Util

---
+++ src/main/java/club/frozed/frozeddisguise/utils/ItemBuilder.java
@@ -0,0 +1,153 @@
+package club.frozed.frozeddisguise.utils;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import org.apache.commons.codec.binary.Base64;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ItemBuilder implements Listener {
+
+    private ItemStack is;
+
+    public ItemBuilder(Material mat) {
+        is = new ItemStack(mat);
+    }
+
+    public ItemBuilder(ItemStack is) {
+        this.is = is;
+    }
+
+    public ItemBuilder amount(int amount) {
+        is.setAmount(amount);
+        return this;
+    }
+
+    public ItemBuilder name(String name) {
+        ItemMeta meta = is.getItemMeta();
+        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+        is.setItemMeta(meta);
+        return this;
+    }
+
+    public ItemBuilder lore(String name) {
+        ItemMeta meta = is.getItemMeta();
+        List<String> lore = meta.getLore();
+
+        if (lore == null) {
+            lore = new ArrayList<>();
+        }
+
+        lore.add(ChatColor.translateAlternateColorCodes('&', name));
+        meta.setLore(lore);
+
+        is.setItemMeta(meta);
+
+        return this;
+    }
+
+    public ItemBuilder lore(String... lore) {
+        List<String> toSet = new ArrayList<>();
+        ItemMeta meta = is.getItemMeta();
+
+        for (String string : lore) {
+            toSet.add(ChatColor.translateAlternateColorCodes('&', string));
+        }
+
+        meta.setLore(toSet);
+        is.setItemMeta(meta);
+
+        return this;
+    }
+
+    public ItemBuilder lore(List<String> lore) {
+        List<String> toSet = new ArrayList<>();
+        ItemMeta meta = is.getItemMeta();
+
+        for (String string : lore) {
+            toSet.add(ChatColor.translateAlternateColorCodes('&', string));
+        }
+
+        meta.setLore(toSet);
+        is.setItemMeta(meta);
+
+        return this;
+    }
+
+    public ItemBuilder durability(int durability) {
+        is.setDurability((short) durability);
+        return this;
+    }
+
+    public ItemBuilder head(String url) {
+/*        ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+        if (url.isEmpty()) return this;*/
+        SkullMeta headMeta = (SkullMeta) is.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+        byte[] encodedData = Base64.encodeBase64(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes());
+        profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
+        Field profileField;
+
+        try {
+            profileField = headMeta.getClass().getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(headMeta, profile);
+        } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e1) {
+            e1.printStackTrace();
+        }
+
+        is.setItemMeta(headMeta);
+        return this;
+    }
+
+    public ItemBuilder enchantment(Enchantment enchantment, int level) {
+        is.addUnsafeEnchantment(enchantment, level);
+        return this;
+    }
+
+    public ItemBuilder enchantment(Enchantment enchantment) {
+        is.addUnsafeEnchantment(enchantment, 1);
+        return this;
+    }
+
+    public ItemBuilder type(Material material) {
+        is.setType(material);
+        return this;
+    }
+
+    public ItemBuilder clearLore() {
+        ItemMeta meta = is.getItemMeta();
+
+        meta.setLore(new ArrayList<>());
+        is.setItemMeta(meta);
+
+        return this;
+    }
+
+    public ItemBuilder clearEnchantments() {
+        for (Enchantment e : is.getEnchantments().keySet()) {
+            is.removeEnchantment(e);
+        }
+
+        return this;
+    }
+
+    public static ItemStack getBackItem() {
+        return (new ItemBuilder(Material.ARROW)).name(ChatColor.RED + "Back").build();
+    }
+
+    public ItemStack build() {
+        return is;
+    }
+
+}
