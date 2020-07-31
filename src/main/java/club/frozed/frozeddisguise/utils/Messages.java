cd0f58e875b6617c1e2d55334e08c919f22ace51
Elb1to <elbitomc@gmail.com>
2020-07-31 16:52:30 -0500

    Created ChatColor translator util

--- src/main/java/club/frozed/frozeddisguise/utils/Messages.java
+++ src/main/java/club/frozed/frozeddisguise/utils/Messages.java
@@ -1,9 +1,21 @@
 package club.frozed.frozeddisguise.utils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Messages {
-    public static String CC(String msg) {
-        return ChatColor.translateAlternateColorCodes('&', msg);
+    public static String CC(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
+
+    public static List<String> CC(List<String> list) {
+        return list.stream().map(Messages::CC).collect(Collectors.toList());
+    }
+
+    public static void sendMessageToPlayer(String message, Player player) {
+        player.sendMessage(Messages.CC(message));
     }
 }
