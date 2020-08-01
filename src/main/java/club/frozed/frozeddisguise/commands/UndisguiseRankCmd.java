342fda2e6a92d1a9cba6d2f376235377daa88f6c
Elb1to <elbitomc@gmail.com>
2020-07-31 20:16:08 -0500

    Created UndisguiseRankCmd

---
+++ src/main/java/club/frozed/frozeddisguise/commands/UndisguiseRankCmd.java
@@ -0,0 +1,37 @@
+package club.frozed.frozeddisguise.commands;
+
+import club.frozed.frozeddisguise.FrozedDisguise;
+import club.frozed.frozeddisguise.managers.PlayerManager;
+import club.frozed.frozeddisguise.utils.Messages;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class UndisguiseRankCmd implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return true;
+        }
+
+        if (!sender.hasPermission("frozed.disguise")) {
+            sender.sendMessage(Messages.CC(FrozedDisguise.getInstance().getConfig().getString("MESSAGES.NO-PERMISSION-TO-USE")));
+            return true;
+        }
+
+        Player player = (Player) sender;
+        if (PlayerManager.rankData.containsKey(player)) {
+            PlayerManager.rankData.remove(player);
+            if (FrozedDisguise.getInstance().getConfig().getBoolean("BOOLEANS.TABLIST_NAME_COLOR")) {
+                player.setPlayerListName(null);
+            }
+            player.sendMessage(Messages.CC(FrozedDisguise.getInstance().getConfig().getString("MESSAGES.DISGUISE-RANK-SUCCESSFULLY-REMOVED")));
+        } else {
+            player.sendMessage(Messages.CC(FrozedDisguise.getInstance().getConfig().getString("MESSAGES.NO-DISGUISE-RANK-ACTIVE")));
+        }
+
+        return false;
+    }
+}
