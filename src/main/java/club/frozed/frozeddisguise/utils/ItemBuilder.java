c0de5c1cdf2431397486483aeeca8bf085e09ea9
Elb1to <elbitomc@gmail.com>
2020-07-31 19:26:10 -0500

    Removed unused method

--- src/main/java/club/frozed/frozeddisguise/utils/ItemBuilder.java
+++ src/main/java/club/frozed/frozeddisguise/utils/ItemBuilder.java
@@ -1,20 +1,14 @@
 package club.frozed.frozeddisguise.utils;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-import org.apache.commons.codec.binary.Base64;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class ItemBuilder implements Listener {
 
@@ -86,27 +80,6 @@ public class ItemBuilder implements Listener {
 
     public ItemBuilder durability(int durability) {
         is.setDurability((short) durability);
-        return this;
-    }
-
-    public ItemBuilder head(String url) {
-/*        ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
-        if (url.isEmpty()) return this;*/
-        SkullMeta headMeta = (SkullMeta) is.getItemMeta();
-        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-        byte[] encodedData = Base64.encodeBase64(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes());
-        profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
-        Field profileField;
-
-        try {
-            profileField = headMeta.getClass().getDeclaredField("profile");
-            profileField.setAccessible(true);
-            profileField.set(headMeta, profile);
-        } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e1) {
-            e1.printStackTrace();
-        }
-
-        is.setItemMeta(headMeta);
         return this;
     }
 
